85fbeeb91e06262d05cce08b43c4d3fa2c190dd5
praveen <prav20ad036@rmkcet.ac.in>
2023-01-28 19:47:19 +0530

    Authentication Added :closed_lock_with_key:

--- myshow/urls.py
+++ myshow/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('/login',views.login,name='login'),
-    path('/regsiter',views.regsiter , name = 'register'),
+    path('login',views.login,name='login'),
+    path('regsiter',views.regsiter , name = 'register'),
     path('movie_page/<movie_name>',views.movie_page,name="movie_page"),
 ]
--- myshow/views.py
+++ myshow/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from .models import user
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
@@ -10,7 +12,31 @@ def movie_page(request,movie_name):
     return render(request,'myshow/movie_page.html',{'movie_name':movie_name})
 
 def login(request):
-    return render(request , 'myshow/login.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(email, password)
+        curr_user = authenticate(email = email, password=password)
+        print(curr_user)
+        if curr_user is not None:
+            login(request, curr_user)
+            return redirect('/index')
+        else:
+            return render(request, 'myshow/login.html', {'error': 'Invalid Credentials'})
+    else:
+        return render(request , 'myshow/login.html')
 
 def regsiter(request):
-    return render(request , 'myshow/register.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('number')
+        password = request.POST.get('pass')
+        Curruser = user(name=name, email=email, phone=phone)
+        authUser = User.objects.create_user(name, email, password)
+        authUser.save()
+        Curruser.save()
+        return redirect('/login')
+    else:
+        return render(request , 'myshow/register.html')
+
