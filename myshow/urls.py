6ad9b736ddae8f05f06f4b52a36e08eb25285843
praveen <prav20ad036@rmkcet.ac.in>
2023-01-29 00:26:54 +0530

    Animated Cards and Authentication updated :100:

--- myshow/urls.py
+++ myshow/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('login',views.login_user,name='login'),
+    path('',views.login_user,name='login'),
+    path('index',views.index,name='index'),
     path('regsiter',views.regsiter , name = 'register'),
+    path('movies',views.movies,name='movies'),
+    path('logout',views.user_logout,name='logout'),
+    path('gifts',views.gifts,name='gifts'),
     path('movie_page/<movie_number>',views.movie_page,name="movie_page"),
 ]
--- myshow/views.py
+++ myshow/views.py
@@ -8,20 +8,22 @@ def index(request):
     return render(request, 'myshow/index.html')
 
 def movie_page(request,movie_number):
-
     print(movie_number)
     ini_path="myshow/images/recommendedMovies/image"+movie_number+".png"
-    return render(request,'myshow/movie_page.html',{'movie_path':ini_path})
+    return render(request,'myshow/movie_page.html',{'movie_path':ini_path })
 
 def login_user(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
+    if request.user.is_authenticated:
+        return redirect('/index')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
         password = request.POST.get('password')
-        print(email, password)
-        curr_user = authenticate(username=email, password=password)
+        print(name, password)
+        curr_user = authenticate(username=name, password=password)
+        print(curr_user)
         if curr_user is not None:
             login(request,curr_user)
-            return redirect('/')
+            return redirect('/index')
         else:
             return render(request, 'myshow/login.html', {'error': 'Invalid Credentials'})
     else:
@@ -33,11 +35,28 @@ def regsiter(request):
         email = request.POST.get('email')
         phone = request.POST.get('number')
         password = request.POST.get('pass')
-        Curruser = user(name=name, email=email, phone=phone)
-        authUser = User.objects.create_user(name, email, password)
-        authUser.save()
-        Curruser.save()
-        return redirect('/login')
+        print(user.objects.filter(name = name).exists())
+        print(name , password)
+        if user.objects.filter(name = name).first():
+            return render(request, 'myshow/register.html', {'error': 'Invalid Credentials'})
+        else:
+            Curruser = user(name=name, email=email, phone=phone)
+            authUser = User.objects.create_user(name, email, password)
+            authUser.save()
+            Curruser.save()
+            return redirect('/login')
+
     else:
         return render(request , 'myshow/register.html')
+
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
+
+def movies(request):
+    return render(request , 'myshow/movies.html')
+
+def gifts(request):
+    return render(request , 'myshow/gifts.html')
 
